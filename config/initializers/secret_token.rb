151794576b3c21df5bd764edbe566afc03f25c55
Henry Happ <henry.happ@prometa.or.cr>
2010-09-27 16:31:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fa87ca0bebfb39f62d9283f50d83b6c787e7edc230601741e7e847b542f95fedecf6ca69b99f76087ac5583790a159c46b52530c704abbd0cda763e8035c0eb9'
