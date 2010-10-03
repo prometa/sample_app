1426fd7fd2b9cf0a5a6d6717d98211e86725a716
Henry Happ <henry.happ@prometa.or.cr>
2010-10-02 21:19:06 -0600

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  # Return the logo
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 
   # Return a title on a per-page basis
   def title
